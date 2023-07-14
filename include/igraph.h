ff93245a49836495d866b8d9cd72b4b15a1bf2be
Maxim Akimenko <maxnet663@gmail.com>
2023-07-14 19:35:44 +0300

    comments added

--- include/igraph.h
+++ include/igraph.h
@@ -3,23 +3,29 @@
 
 #include <vector>
 
-class IGraph {
+class IGraph { // interface
 public:
     virtual ~IGraph() {}
     IGraph() {};
     IGraph(IGraph *_oth) {};
 
-    // Метод принимает вершины начала и конца ребра и добавляет ребро
+    // The method takes the vertices of the beginning 
+    // and end of the edge and adds the edge
     virtual void AddEdge(int from, int to) = 0;
 
-    // Метод должен считать текущее количество вершин
+    // The method counts the current number of vertices
     virtual int VerticesCount() const = 0;
 
-    // Для конкретной вершины метод выводит в вектор «вершины» все вершины, в которые можно дойти по ребру из данной
-    virtual void GetNextVertices(int vertex, std::vector<int> &vertices) const = 0;
+    // For a specific vertex, the method outputs to the "vertex" vector
+    // all vertices that can be reached along the edge from the given
+    virtual void GetNextVertices(int vertex
+                                , std::vector<int> &vertices) const = 0;
 
-    // Для конкретной вершины метод выводит в вектор «вершины» все вершины, из которых можно дойти по ребру в данную
-    virtual void GetPrevVertices(int vertex, std::vector<int> &vertices) const = 0;
+    // For a specific vertex, the method outputs to the "vertex" vector 
+    // all vertices from which it is possible to walk along the edge 
+    // to the given
+    virtual void GetPrevVertices(int vertex
+                                , std::vector<int> &vertices) const = 0;
 };
 
 #endif //IGRAPH_H_SENTRY
